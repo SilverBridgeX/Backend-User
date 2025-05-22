596030b704b4b29101474c863bfab2501b122c95
choyoungseo20 <whdudtj0809@naver.com>
2025-05-22 21:24:07 +0900

    health-check API 생성

---
+++ src/main/java/com/example/silverbridgeX_user/SilverbridgeXUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.silverbridgeX_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverbridgeXUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilverbridgeXUserApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/silverbridgeX_user/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.example.silverbridgeX_user.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI SILVERBRIDGEXUSERAPI() {
+        Info info = new Info()
+                .title("SILVERBRIDGEX-USER API")
+                .description("SILVERBRIDGEX-USER 명세서")
+                .version("1.0.0");
+
+        String jwtSchemeName = "JWT TOKEN";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+        //.type(SecurityScheme.Type.APIKEY) // API 키 방식
+        //.in(SecurityScheme.In.HEADER) // 헤더에 포함
+        //.name("Authorization")); // 헤더의 이름
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
