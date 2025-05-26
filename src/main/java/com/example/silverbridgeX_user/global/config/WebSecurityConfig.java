f2036e33891f2bf137b5c0f50d77192052d842b3
persi <jina8151010@gmai.com>
2025-05-27 01:05:59 +0900

    config: Spring Security config

---
+++ src/main/java/com/example/silverbridgeX_user/global/config/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.silverbridgeX_user.global.config;
+
+import com.example.silverbridgeX_user.user.jwt.AuthCreationFilter;
+import com.example.silverbridgeX_user.user.jwt.JwtValidationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+    private final AuthCreationFilter authCreationFilter;
+    private final JwtValidationFilter jwtValidationFilter;
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authHttp -> authHttp
+                        .requestMatchers(
+                                "/user/health", // health check
+                                "/swagger-ui/**", // swagger
+                                "/v3/api-docs/**"
+                        )
+                        .permitAll()
+                        .anyRequest().permitAll()
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(authCreationFilter, AuthorizationFilter.class)
+                .addFilterBefore(jwtValidationFilter, AuthCreationFilter.class);
+
+        return http.build();
+    }
+}
