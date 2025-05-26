0ac607f6427ebc188bee2073c7bedf2b0ded54cd
persi <jina8151010@gmai.com>
2025-05-27 01:42:40 +0900

    core: neo4j 설정 보류

--- src/main/java/com/example/silverbridgeX_user/global/config/Neo4jConfig.java
+++ src/main/java/com/example/silverbridgeX_user/global/config/Neo4jConfig.java
@@ -1,26 +1,21 @@
 package com.example.silverbridgeX_user.global.config;
 
-import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.Driver;
-import org.neo4j.driver.GraphDatabase;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Neo4jConfig {
 
-    @Value("${org.neo4j.driver.uri}")
-    private String uri;
-
-    @Value("${org.neo4j.driver.authentication.username}")
-    private String username;
-
-    @Value("${org.neo4j.driver.authentication.password}")
-    private String password;
-
-    @Bean
-    public Driver neo4jDriver() {
-        return GraphDatabase.driver(uri, AuthTokens.basic(username, password));
-    }
+//    @Value("${org.neo4j.driver.uri}")
+//    private String uri;
+//
+//    @Value("${org.neo4j.driver.authentication.username}")
+//    private String username;
+//
+//    @Value("${org.neo4j.driver.authentication.password}")
+//    private String password;
+//
+//    @Bean
+//    public Driver neo4jDriver() {
+//        return GraphDatabase.driver(uri, AuthTokens.basic(username, password));
+//    }
 }
