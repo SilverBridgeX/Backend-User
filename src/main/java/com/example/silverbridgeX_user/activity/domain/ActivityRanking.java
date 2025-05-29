b2e439f583e418ae132dd3cd3e18d99f3b104b94
persi <jina8151010@gmai.com>
2025-05-29 23:02:44 +0900

    core: 활동 랭킹 저장 위한 테이블 추가

---
+++ src/main/java/com/example/silverbridgeX_user/activity/domain/ActivityRanking.java
@@ -0,0 +1,33 @@
+package com.example.silverbridgeX_user.activity.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ActivityRanking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity_id")
+    private Activity activity;
+
+    private Integer rank;
+
+    private LocalDate createdAt;
+}
