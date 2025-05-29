41d0f46ba40b44a0dc76f47ac824b70ec380f3b2
persi <jina8151010@gmai.com>
2025-05-29 15:58:09 +0900

    core: 필요없어진 persona 엔티티 삭제

--- src/main/java/com/example/silverbridgeX_user/user/repository/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.silverbridgeX_user.user.repository;
-
-import com.example.silverbridgeX_user.user.domain.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-}
