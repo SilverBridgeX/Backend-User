6eb74c4bc88b7c74bc3207f22055b54c3a40a390
persi <jina8151010@gmai.com>
2025-06-26 17:54:18 +0900

    refactor: 사용하지 않는 코드 제거

--- src/main/java/com/example/silverbridgeX_user/payment/service/PaymentService.java
+++ src/main/java/com/example/silverbridgeX_user/payment/service/PaymentService.java
@@ -8,6 +8,12 @@ import com.example.silverbridgeX_user.payment.dto.PaymentDto;
 import com.example.silverbridgeX_user.payment.repository.PaymentRepository;
 import com.example.silverbridgeX_user.user.domain.User;
 import com.example.silverbridgeX_user.user.repository.UserRepository;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -16,13 +22,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -60,10 +59,12 @@ public class PaymentService {
         parameters.put("total_amount", "9900");
         parameters.put("vat_amount", "200");
         parameters.put("tax_free_amount", "0");
-        parameters.put("approval_url", "http://15.165.17.95/user/payment/success"); // http://15.165.17.95/user/payment/success http://localhost:8080/payment/success
-        parameters.put("fail_url", "http://15.165.17.95/user/payment/fail"); // http://15.165.17.95/user/payment/fail http://localhost:8080/payment/fail
-        parameters.put("cancel_url", "http://15.165.17.95/user/payment/cancel"); // http://15.165.17.95/user/payment/cancel http://localhost:8080/payment/cancel
-
+        parameters.put("approval_url",
+                "http://15.165.17.95/user/payment/success"); // http://15.165.17.95/user/payment/success http://localhost:8080/payment/success
+        parameters.put("fail_url",
+                "http://15.165.17.95/user/payment/fail"); // http://15.165.17.95/user/payment/fail http://localhost:8080/payment/fail
+        parameters.put("cancel_url",
+                "http://15.165.17.95/user/payment/cancel"); // http://15.165.17.95/user/payment/cancel http://localhost:8080/payment/cancel
 
         HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(parameters, this.getHeaders());
 
@@ -113,7 +114,8 @@ public class PaymentService {
     }
 
     public Payment getKakaoPayInfo(Long userId) {
-        Payment kakaoPay =  paymentRepository.findByUser_Id(userId).orElseThrow(() -> new GeneralException(ErrorCode.TID_NOT_EXIST));
+        Payment kakaoPay = paymentRepository.findByUser_Id(userId)
+                .orElseThrow(() -> new GeneralException(ErrorCode.TID_NOT_EXIST));
 
         return kakaoPay;
     }
@@ -126,10 +128,11 @@ public class PaymentService {
         boolean isPremium = false;
 
         if (paymentRepository.existsByUser_Id(userId)) {
-            Payment kakaoPay =  paymentRepository.findByUser_Id(userId).orElseThrow(() -> new GeneralException(ErrorCode.TID_NOT_EXIST));
+            Payment kakaoPay = paymentRepository.findByUser_Id(userId)
+                    .orElseThrow(() -> new GeneralException(ErrorCode.TID_NOT_EXIST));
 
-            if (kakaoPay.getSid() == null || kakaoPay.getSid().isEmpty()) {}
-            else {
+            if (kakaoPay.getSid() == null || kakaoPay.getSid().isEmpty()) {
+            } else {
                 Map<String, Object> parameters = new HashMap<>();
                 parameters.put("cid", cid);
                 parameters.put("sid", kakaoPay.getSid());
@@ -143,13 +146,14 @@ public class PaymentService {
 
                 if (kakaoSubscribeStatusResponse.getStatus().equals("ACTIVE")) {
                     isPremium = true;
-                }
-                else {
+                } else {
                     String last_approved_at;
-                    if(kakaoSubscribeStatusResponse.getLast_approved_at() == null || kakaoSubscribeStatusResponse.getLast_approved_at().isEmpty()) {
+                    if (kakaoSubscribeStatusResponse.getLast_approved_at() == null
+                            || kakaoSubscribeStatusResponse.getLast_approved_at().isEmpty()) {
                         last_approved_at = kakaoSubscribeStatusResponse.getCreated_at();
+                    } else {
+                        last_approved_at = kakaoSubscribeStatusResponse.getLast_approved_at();
                     }
-                    else last_approved_at = kakaoSubscribeStatusResponse.getLast_approved_at();
 
                     DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
                     LocalDateTime lastApprovedAt = LocalDateTime.parse(last_approved_at, formatter);
@@ -233,11 +237,12 @@ public class PaymentService {
     public void saveTid(Long userId, String tid) {
         Payment kakaoPay;
         if (paymentRepository.existsByUser_Id(userId)) {
-            kakaoPay = paymentRepository.findByUser_Id(userId).orElseThrow(() -> new GeneralException(ErrorCode.TID_SID_UNSUPPORTED));
+            kakaoPay = paymentRepository.findByUser_Id(userId)
+                    .orElseThrow(() -> new GeneralException(ErrorCode.TID_SID_UNSUPPORTED));
             kakaoPay.updateTid(tid);
-        }
-        else {
-            User user = userRepository.findById(userId).orElseThrow(() -> new GeneralException(ErrorCode.USER_NOT_FOUND));
+        } else {
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new GeneralException(ErrorCode.USER_NOT_FOUND));
             kakaoPay = PaymentConverter.toKakaoPayTid(tid, user);
         }
         paymentRepository.save(kakaoPay);
@@ -245,7 +250,8 @@ public class PaymentService {
 
     public void saveSid(PaymentDto.KakaoApproveResponse kakaoApproveResponse) {
 
-        Payment kakaoPay = paymentRepository.findByTid(kakaoApproveResponse.getTid()).orElseThrow(() -> new GeneralException(ErrorCode.TID_NOT_EXIST));
+        Payment kakaoPay = paymentRepository.findByTid(kakaoApproveResponse.getTid())
+                .orElseThrow(() -> new GeneralException(ErrorCode.TID_NOT_EXIST));
 
         kakaoPay.updateSid(kakaoApproveResponse.getSid());
 
@@ -255,18 +261,21 @@ public class PaymentService {
     public void savePayInfo(Long userId, PaymentDto.KakaoApproveResponse kakaoApproveResponse) {
         Payment kakaoPay;
         if (paymentRepository.existsByUser_Id(userId)) {
-            kakaoPay = paymentRepository.findByUser_Id(userId).orElseThrow(() -> new GeneralException(ErrorCode.TID_SID_UNSUPPORTED));
+            kakaoPay = paymentRepository.findByUser_Id(userId)
+                    .orElseThrow(() -> new GeneralException(ErrorCode.TID_SID_UNSUPPORTED));
             kakaoPay.updatePayInfo(kakaoApproveResponse.getTid(), kakaoApproveResponse.getSid());
-        }
-        else {
-            User member = userRepository.findById(userId).orElseThrow(() -> new GeneralException(ErrorCode.USER_NOT_FOUND));
-            kakaoPay = PaymentConverter.toKakaoPay(kakaoApproveResponse.getTid(), kakaoApproveResponse.getSid(), member);
+        } else {
+            User member = userRepository.findById(userId)
+                    .orElseThrow(() -> new GeneralException(ErrorCode.USER_NOT_FOUND));
+            kakaoPay = PaymentConverter.toKakaoPay(kakaoApproveResponse.getTid(), kakaoApproveResponse.getSid(),
+                    member);
         }
         paymentRepository.save(kakaoPay);
     }
 
     public void cancelPay(Long userId) {
-        Payment kakaoPay = paymentRepository.findByUser_Id(userId).orElseThrow(() -> new GeneralException(ErrorCode.TID_NOT_EXIST));
+        Payment kakaoPay = paymentRepository.findByUser_Id(userId)
+                .orElseThrow(() -> new GeneralException(ErrorCode.TID_NOT_EXIST));
 
         paymentRepository.delete(kakaoPay);
     }
@@ -277,7 +286,8 @@ public class PaymentService {
         kakaoPayList.stream()
                 .forEach(kakaoPay -> {
                     if (kakaoPay.getSid() != null && !kakaoPay.getSid().isEmpty()) {
-                        PaymentDto.KakaoSubscribeStatusResponse kakaoSubscribeStatusResponse = subscribeStatusResponse(kakaoPay.getSid());
+                        PaymentDto.KakaoSubscribeStatusResponse kakaoSubscribeStatusResponse = subscribeStatusResponse(
+                                kakaoPay.getSid());
 
                         // "ACTIVE" 상태인지 확인
                         if (kakaoSubscribeStatusResponse.getStatus().equals("ACTIVE")) {
@@ -294,21 +304,16 @@ public class PaymentService {
 
                             // 결제일과 오늘의 일(day)이 같고, 마지막 결제일이 이번 달이 아닌 경우에만 결제 수행
                             if (today.getDayOfMonth() == lastApprovedAt.getDayOfMonth() &&
-                                    (today.getYear() != lastApprovedAt.getYear() || today.getMonthValue() != lastApprovedAt.getMonthValue())) {
+                                    (today.getYear() != lastApprovedAt.getYear()
+                                            || today.getMonthValue() != lastApprovedAt.getMonthValue())) {
 
-                                PaymentDto.KakaoApproveResponse approveResponse = approveSubscribeResponse(kakaoPay.getSid());
+                                PaymentDto.KakaoApproveResponse approveResponse = approveSubscribeResponse(
+                                        kakaoPay.getSid());
 
                                 savePayInfo(kakaoPay.getUser().getId(), approveResponse);
                             }
-//                            if (today.getDayOfMonth() == lastApprovedAt.getDayOfMonth()) {
-//                                KakaoPayDTO.KakaoApproveResponse approveResponse = approveSubscribeResponse(kakaoPay.getSid());
-//
-//                                savePayInfo(kakaoPay.getMember().getId(), approveResponse);
-//                            }
                         }
                     }
                 });
-
-//        System.out.println("정기 결제 작업 완료");
     }
 }
